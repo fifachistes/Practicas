654373b4068a96cd35691028185a2c54f515b08a
fifachistes <147640955+fifachistes@users.noreply.github.com>
2023-10-18 17:27:55 +0200

    Create ejercicio18octubre.cpp

---
+++ ejercicio18octubre.cpp
@@ -0,0 +1,79 @@
+// ejercicio 5
+//
+
+#include <iostream>
+using namespace std;
+
+int funcionAlfa(char caracter) {
+
+    string minusculas = "abcdefghijklmnopqrstvwxyz";
+    string numeros = "0123456789";
+    string mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (int i = 0; i < 26; i++) {
+
+        if (caracter == minusculas[i]) {
+            return 1;
+        }
+        if (caracter == mayusculas[i]) {
+            return 2;
+        }
+        if (i <= 10 && (caracter == numeros[i])) {
+            return 3;
+        }
+
+    }
+
+    return 4;
+}
+
+int main()
+{
+    char caracter;
+
+    cout << " Mete un numero" << endl;
+
+    cin >> caracter;
+
+    int minusculas = 0;
+    int mayusculas = 0;
+    int numeros = 0;
+    int otros = 0;
+
+    while (caracter != '*') {
+        int tipo = funcionAlfa(caracter);
+
+        if (tipo == 1) {
+            minusculas += 1;
+        }
+        if (tipo == 2) {
+            mayusculas += 1;
+        }
+        if (tipo == 3) {
+            numeros += 1;
+        }
+        if (tipo == 4) {
+            otros += 1;
+        }
+
+        cin >> caracter;
+    }
+
+    cout << "Minusculas : " << minusculas << endl;
+    cout << "Mayusculas : " << mayusculas << endl;
+    cout << "Numeros : " << numeros << endl;
+    cout << "Otros : " << otros << endl;
+
+    return 0;
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
